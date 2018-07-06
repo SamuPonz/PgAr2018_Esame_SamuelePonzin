934e78b6fbd7ab7a8c3f058940fcb8a8360fdadc
Samuele <s4mu98@gmail.com>
2018-07-06 14:38:43 +0200

    second commit

--- src/program/AvventureGraficheMain.java
+++ src/program/AvventureGraficheMain.java
@@ -4,7 +4,12 @@ public class AvventureGraficheMain {
 
 	public static void main(String[] args) {
 		
-
+		Story storia = new Story();
+		Utility.read("PgAr2018_Story_2.1.xml", storia);
+		for(Paragraph chap : storia.getChapters()) {
+			System.out.println(chap.printChapter());
+			
+		}
 	}
 
 }
